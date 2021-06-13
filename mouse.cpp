23de1711513320653a8169d60c03a98be5d0dad5
isanthree <h1486636414@163.com>
2021-06-12 20:30:35 -0700

    can move mouse now

--- mouse.cpp
+++ mouse.cpp
@@ -10,10 +10,11 @@ MouseDriver::MouseDriver(InterruptManager* manager)
     y(12)
 {
     uint16_t* VideoMemory = (uint16_t*)0xb8000;  // 获取内存地址
-    // 往这个内存地址做一些操作，这样可以获取到内存对应的位置
+    // 往这个内存地址做一些操作，这样可以获取到鼠标对应的位置
     VideoMemory[y*80+x] = ((VideoMemory[y*80+x] & 0xf000) >> 4) |
                           ((VideoMemory[y*80+x] & 0x0f00) << 4) |
                           (VideoMemory[y*80+x] & 0x00ff);
+    
     commandport.Write(0xa8);
     commandport.Write(0x20);
     uint8_t status = (dataport.Read() | 2) & ~0x20;
@@ -27,10 +28,47 @@ MouseDriver::MouseDriver(InterruptManager* manager)
 MouseDriver::~MouseDriver() {}
 
 void printf(const char*);
+
 uint32_t MouseDriver::HandleInterrupt(uint32_t esp)
 {
     uint8_t status = commandport.Read();  // 首先从 commandport 里面读取鼠标的状态
     if (!(status & 0x20)) return esp;  // 如果不是鼠标则直接返回；0x20 对应着上面 0x20 相关状态值的操作
     
+    buffer[offset] = dataport.Read();  // 从 buffer 里面读取数据
+    offset = (offset + 1) % 3;  // 然后 offset 此时需要往后偏移一位
+
+    if (offset == 0)  // 如果 offset==0，说明 x,y 被操纵过了
+    {
+        uint16_t* VideoMemory = (uint16_t*)0xb8000;  // 获取内存地址
+        // 往这个内存地址做一些操作，这样可以获取到鼠标对应的位置
+        VideoMemory[y*80+x] = ((VideoMemory[y*80+x] & 0xf000) >> 4) |
+                              ((VideoMemory[y*80+x] & 0x0f00) << 4) |
+                              (VideoMemory[y*80+x] & 0x00ff);
+        // 鼠标往右移动：x 增大；鼠标往下移动：y 增大，但是 buffer[2] 是相反的，取一下反
+        x += buffer[1];
+        if (x < 0) x = 0;
+        else if (x >= 80) x = 79;
+
+        y -= buffer[2];
+        if (y < 0) y = 0;
+        else if (y >= 25) y = 24;
+
+        VideoMemory[y*80+x] = ((VideoMemory[y*80+x] & 0xf000) >> 4) |
+                              ((VideoMemory[y*80+x] & 0x0f00) << 4) |
+                              (VideoMemory[y*80+x] & 0x00ff);
+
+        // 实现按键按下变白的功能
+        for(uint8_t i=0; i<3; i++)
+        {
+            if ((buffer[0] & (1 << i)) != (buttons & (1 << i)))
+            {
+                VideoMemory[y*80+x] = ((VideoMemory[y*80+x] & 0xf000) >> 4) |
+                              ((VideoMemory[y*80+x] & 0x0f00) << 4) |
+                              (VideoMemory[y*80+x] & 0x00ff);
+            }
+        }
+        buttons = buffer[0];
+    }
+
     return esp;
 }
